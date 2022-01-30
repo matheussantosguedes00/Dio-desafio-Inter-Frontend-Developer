18eef766136f38595252f022d7dc289e0c5a6f90
Matheus Santos Guedes <68879085+matheussantosguedes00@users.noreply.github.com>
2022-01-29 21:21:38 -0300

    up

---
+++ Sintaxe Básica em JavaScript/Aula 2-Entendendo variáveis e seus valores/script.js
@@ -0,0 +1,145 @@
+// Tipos primitivos
+//boolear
+var vOuf=false;
+console.log(vOuf);
+console.log(typeof(vOuf));
+
+//number;
+var numeroQualquer =1;
+console.log(numeroQualquer);
+console.log(typeof(numeroQualquer));
+
+//string
+var nome= 'diana';
+console.log(nome);
+console.log(typeof(nome));
+
+//como declarar
+var variavel= 'diana';
+variavel ='martine';
+console.log(variavel);
+
+let variavel2='diana';
+variavel2 ='martine';
+console.log(variavel2);
+
+const constante ='diana';
+console.log(constante);
+
+var escopoGlobal='global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno ='local';
+    console.log(escopoLocalInterno);
+}
+escopoLocal();
+
+//atribuição 
+var atribuicao ='diana';
+
+//comparacao 
+var comparacao = '0'== 0;
+console.log(comparacao);
+
+//comparacao Identica
+var comparacaoIdentica ='0'===0;
+console.log(comparacaoIdentica);
+
+//adicao
+var adicao =1 + 1;
+console.log(adicao);
+
+//subtracao
+var subtracao =2 - 1;
+console.log(subtracao);
+
+//multiplicacao
+var multiplicacao =2 * 2;
+console.log(multiplicacao);
+
+//divisão Real
+var divisaoReal =6/2;
+console.log(divisaoReal);
+
+//divisão Inteira
+var divisãoInteira=5 % 2;
+console.log(divisãoInteira);
+
+//Potenciacao
+var Potenciacao=2**10;
+console.log(Potenciacao);
+
+// maior que
+var maiorQue=5>2;
+console.log(maiorQue);
+
+//menor que
+var menorQue =5<2;
+console.log(menorQue);
+
+//maior Ou Igual
+var maiorOuIgual=5>=2;
+console.log(maiorOuIgual);
+
+//menor Ou  Igual
+var menorOuIgual=5<=2;
+console.log(menorOuIgual);
+
+var e = true &true;
+console.log(e);
+
+var ou =true || false;
+console.log(ou);
+
+var nao=!true;
+console.log(nao);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
